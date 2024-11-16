162dbbc04d31a3cb251efa93851597f61a573262
asunismichela@gmail.com <asunismichela@gmail.com>
2024-11-16 09:01:08 +0000

    purchase cost and margin

--- models/int_sales_margin.sql
+++ models/int_sales_margin.sql
@@ -1,8 +1,13 @@
 SELECT 
+products_id,
+purchase_price,
 quantity,
-purchase_price
+revenue,
+(quantity * purchase_price) as purchase_cost,
+(revenue - (quantity * purchase_price)) as margin
 FROM
-{{ref("sales")}} as sales
+{{ref("stg_raw__sales")}} as sales
 JOIN
-{{ref("product") }} as product
-  ON sales.products_id = product.products_id
+{{ref("stg_raw__product") }} as product
+  USING (products_id)
+  ORDER BY margin ASC
